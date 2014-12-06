ee0851b77774f5cab07b003de25f79d45d3962a3
rgsouthall <rgsouthall@gmail.com>
2014-12-06 19:22:56 +0000

    Recommit of the previous fix

--- __init__.py
+++ __init__.py
@@ -40,19 +40,14 @@ evsep = {'linux': ':', 'darwin': ':', 'win32': ';'}
 
 
 if 'RAYPATH' not in os.environ:
-    print('hi')
     radldir = [d for d in rplatldict[str(sys.platform)] if os.path.isdir(d)]
     radbdir = [d for d in rplatbdict[str(sys.platform)] if os.path.isdir(d)]
     epdir = eplatbdict[str(sys.platform)] if os.path.isdir(eplatbdict[str(sys.platform)]) else os.path.join('{}'.format(addonpath), 'EPFiles', 'bin',  platdict[str(sys.platform)])
     if epdir == eplatbdict[str(sys.platform)] and os.path.isfile(os.path.join(eplatbdict[str(sys.platform)], 'Energy+.idd')):
         shutil.copyfile(os.path.join(eplatbdict[str(sys.platform)], 'Energy+.idd'), os.path.join('{}'.format(addonpath), 'EPFiles', 'Energy+.idd'))            
     if not radldir:
-        print('hi2')
         radbdir, radldir = [os.path.join('{}'.format(addonpath), 'Radfiles', 'bin', platdict[str(sys.platform)])], [os.path.join('{}'.format(addonpath), 'Radfiles', 'lib')]
-        os.environ["RAYPATH"] = radldir[0]
-    else:
-        print('{0}{1}{2}'.format(radldir[0], evsep[str(sys.platform)], os.path.join(addonpath, 'lib')))
-        os.environ["RAYPATH"] = '{0}{1}{2}'.format(radldir[0], evsep[str(sys.platform)], os.path.join(addonpath, 'lib'))        
+    os.environ["RAYPATH"] = '{0}{1}{2}'.format(radldir[0], evsep[str(sys.platform)], os.path.join(addonpath, 'lib'))        
     os.environ["PATH"] = os.environ["PATH"] + "{0}{1}{0}{2}".format(evsep[str(sys.platform)], radbdir[0], epdir)    
 else:
     os.environ["RAYPATH"] += '{0}{1}'.format(evsep[str(sys.platform)], os.path.join(addonpath, 'lib'))

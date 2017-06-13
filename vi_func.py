2f7dfbaf0408f0651b776744b9b279761a59698b
Ryan Southall <rgsouthall@gmail.com>
2017-06-13 12:30:59 +0100

    Update Energyplus version to 8.7

--- vi_func.py
+++ vi_func.py
@@ -1572,7 +1572,7 @@ def viparams(op, scene):
     if not scene.get('enparams'):
         scene['enparams'] = {}
     scene['enparams']['idf_file'] = idf
-    scene['enparams']['epversion'] = '8.6'
+    scene['enparams']['epversion'] = '8.7'
     if not scene.get('flparams'):
         scene['flparams'] = {}
     scene['flparams']['offilebase'] = offb

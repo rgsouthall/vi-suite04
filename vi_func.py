6f957ec79ad3fe8b0d21ff7a7eb5b466c526ad8c
Ryan <rgsouthall@gmail.com>
2018-04-30 16:37:37 +0100

    Fixes for HDR mapping and Rvu camera limits

--- vi_func.py
+++ vi_func.py
@@ -481,7 +481,7 @@ def cbdmhdr(node, scene):
     return skyentry
 
 def hdrsky(hdrfile, hdrmap, hdrangle, hdrradius):
-    hdrangle = '1 {:.3f}'.format(hdrangle * math.pi/180) if hdrangle else '1 0'
+    hdrangle = '1 {:.3f}'.format(hdrangle * math.pi/180 + math.pi * 0.5)# if hdrangle else '1 0'
     hdrfn = {'0': 'sphere2latlong', '1': 'sphere2angmap'}[hdrmap]
     return("# Sky material\nvoid colorpict hdr_env\n7 red green blue '{}' {}.cal sb_u sb_v\n0\n{}\n\nhdr_env glow env_glow\n0\n0\n4 1 1 1 0\n\nenv_glow bubble sky\n0\n0\n4 0 0 0 {}\n\n".format(hdrfile, hdrfn, hdrangle, hdrradius))
 
